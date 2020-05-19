049b9386267b4c2704108ed2bb959c2967b1f5f1
Jonathan Sabbagh <jb.sabbagh1@gmail.com>
2020-05-18 20:59:17 -0400

    refactor: Refactored to connect to database using a config file
    To avoid storing the database credentials in the actual source code,
    I created a db.cfg file that stores that information. In the future,
    I will explore more secure ways to store credentials.

--- create_tables.py
+++ create_tables.py
@@ -1,27 +1,27 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+# Constants
+CONFIG_FILE = "db.cfg"
 
-def create_database():
+def create_database_connection(config_file=CONFIG_FILE):
     """
-    - Creates and connects to the sparkifydb
+    - Connects to the database
     - Returns the connection and cursor to sparkifydb
-    """
-    
-    # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
-    
-    # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    """    
+    import configparser
 
-    # close connection to default database
-    conn.close()    
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    
+    # Credentials
+    HOST = config.get("POSTGRES", "HOST")
+    DB_NAME = config.get("POSTGRES", "DB_NAME")
+    USER = config.get("POSTGRES", "USER")
+    PASSWORD = config.get("POSTGRES", "PASSWORD")
     
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(f"host={HOST} dbname={DB_NAME} user={USER} password={PASSWORD}")
     cur = conn.cursor()
     
     return cur, conn
@@ -58,7 +58,7 @@ def main():
     
     - Finally, closes the connection. 
     """
-    cur, conn = create_database()
+    cur, conn = create_database_connection()
     
     drop_tables(cur, conn)
     create_tables(cur, conn)
